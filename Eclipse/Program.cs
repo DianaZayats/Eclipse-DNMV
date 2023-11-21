3724f7c0396b1671949ca06ee89d832c863e2329
Pugach-Nazar <qwertyasd0933@gmail.com>
2023-11-21 17:00:07 +0200

    Add project files.

---
+++ Eclipse/Program.cs
@@ -0,0 +1,26 @@
+﻿using Eclipse;
+using System;
+
+namespace Program
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            var pwd = new PasswordHashing();
+
+            pwd.GenerateSalt();
+            var salt = pwd.salt;
+
+            var hash  = pwd.HashPassword("password");
+            Console.WriteLine(hash);
+            Console.WriteLine();
+            hash = pwd.HashPassword("Password");
+            Console.WriteLine(hash);
+            Console.WriteLine();
+            hash = pwd.HashPassword("Password");
+            Console.WriteLine(hash);
+        }
+    }
+}
