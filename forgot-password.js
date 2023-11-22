8cc6af44775394f79ec26ca1236bcd04859b6d75
Nikita Shevchenko <58083521+AncleMarvel@users.noreply.github.com>
2023-11-22 17:23:38 +0200

    fix(forgot): pass toggling

--- forgot-password.js
+++ forgot-password.js
@@ -1,24 +1,26 @@
-const passwordInput = document.getElementById("password");
-const togglePassword = document.getElementById("togglePassword");
-const loginButton = document.querySelector(".btn");
 
-if (togglePassword) {
-  togglePassword.addEventListener("click", () => {
-    if (passwordInput.type === "password") {
-      passwordInput.type = "text";
-      togglePassword.innerHTML = '<i class="far fa-eye"></i>';
-    } else {
-      passwordInput.type = "password";
-      togglePassword.innerHTML = '<i class="far fa-eye-slash"></i>';
-    }
-  });
-}
+const showPasswordButtons = document.querySelectorAll(".showPassword");
+
+showPasswordButtons.forEach(btn => {
+  if (btn) {
+    btn.addEventListener("click", () => {
+      const passwordInput = document.querySelector(`#${btn.dataset.for}`);
+      
+      if (passwordInput.type === "password") {
+        passwordInput.type = "text";
+        btn.innerHTML = '<i class="far fa-eye"></i>';
+      } else {
+        passwordInput.type = "password";
+        btn.innerHTML = '<i class="far fa-eye-slash"></i>';
+      }
+    });
+  }
+}); 
 
 const passwordResettingFrom = document.getElementById("ForgotPasswordForm");
 if (passwordResettingFrom) {
   passwordResettingFrom.addEventListener("submit", (e) => {
     e.preventDefault();
-    console.log('✌️e --->', e);
     window.location.href = "personalinfo.html";
   });
 }
