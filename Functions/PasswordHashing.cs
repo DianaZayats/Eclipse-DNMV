adb6b876905e5859d3a9e14ead81730bd7d36d7c
mulykoV <144117863+mulykoV@users.noreply.github.com>
2023-11-23 03:30:49 +0200

    Add files via upload
    YEEEESSSSSSSSS. WE will win!!!!!

---
+++ Functions/PasswordHashing.cs
@@ -0,0 +1,30 @@
+﻿using System.Security.Cryptography;
+
+namespace Eclipse
+{
+    internal class PasswordHashing
+    {
+        public string salt { get; set; }
+
+        private readonly int iterarions = 10000;
+        private readonly int hashLenght = 64;
+        public void GenerateSalt()
+        {
+            using (var randomNumberGenerator = new RNGCryptoServiceProvider())
+            {
+                var randomNumber = new byte[32];
+                randomNumberGenerator.GetBytes(randomNumber);
+                salt = Convert.ToBase64String(randomNumber);
+            }
+
+        }
+
+        public string HashPassword(string toBeHashed)
+        {
+            using (var rfc2898 = new Rfc2898DeriveBytes(toBeHashed, Convert.FromBase64String(salt), iterarions, HashAlgorithmName.SHA512))
+            {
+                return Convert.ToBase64String(rfc2898.GetBytes(hashLenght));
+            }
+        }
+    }
+}
