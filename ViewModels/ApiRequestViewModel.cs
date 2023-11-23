98648eae8d0d82ee0e8faf96c21896a543850c11
mulykoV <144117863+mulykoV@users.noreply.github.com>
2023-11-23 12:37:09 +0200

    Delete ViewModels directory

--- ViewModels/ApiRequestViewModel.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace RegistrationUserVova.ViewModels
-{
-    public class ApiRequestViewModel
-    {
-        public string EndpointUrl { get; set; }
-        public string HttpMethod { get; set; } 
-        public object Payload { get; set; }
-    }
-}
